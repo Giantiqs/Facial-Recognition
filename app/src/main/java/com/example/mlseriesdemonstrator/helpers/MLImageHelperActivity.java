0d5b77e4831ddcbd0df28e611292dde8668e0256
gian <michaelgiantiqui3@gmail.com>
2023-06-16 16:58:27 +0800

    replaced list with hashmap in FaceRecognitionProcessor

--- app/src/main/java/com/example/mlseriesdemonstrator/helpers/MLImageHelperActivity.java
+++ app/src/main/java/com/example/mlseriesdemonstrator/helpers/MLImageHelperActivity.java
@@ -7,7 +7,6 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.ColorSpace;
 import android.graphics.ImageDecoder;
 import android.graphics.Matrix;
 import android.graphics.Paint;
