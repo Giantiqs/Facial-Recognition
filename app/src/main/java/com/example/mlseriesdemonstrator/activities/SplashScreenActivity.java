91429e6fdb8949b8b3c1741016701c142fd494ef
gian <michaelgiantiqui3@gmail.com>
2023-06-25 23:11:19 +0800

    splash screen activity

---
+++ app/src/main/java/com/example/mlseriesdemonstrator/activities/SplashScreenActivity.java
@@ -0,0 +1,16 @@
+package com.example.mlseriesdemonstrator.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mlseriesdemonstrator.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
