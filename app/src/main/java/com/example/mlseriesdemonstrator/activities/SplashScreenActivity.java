2f6602fa1a340a73269961126cfccb2a8da61a37
gian <michaelgiantiqui3@gmail.com>
2023-06-26 14:00:45 +0800

    Splash screen initial design

--- app/src/main/java/com/example/mlseriesdemonstrator/activities/SplashScreenActivity.java
+++ app/src/main/java/com/example/mlseriesdemonstrator/activities/SplashScreenActivity.java
@@ -3,14 +3,20 @@ package com.example.mlseriesdemonstrator.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ProgressBar;
 
 import com.example.mlseriesdemonstrator.R;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
+    ProgressBar circleProgressBar, horizontalProgressBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        circleProgressBar = findViewById(R.id.PROGRESS_BAR_CIRCLE);
+        horizontalProgressBar = findViewById(R.id.PROGRESS_BAR_HORIZONTAL);
     }
 }
