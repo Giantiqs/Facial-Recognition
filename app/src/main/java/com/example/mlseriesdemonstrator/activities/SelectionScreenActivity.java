28d94b6ed0fa0344dd07bb668c3343dd5cbfc181
gian <michaelgiantiqui3@gmail.com>
2023-06-26 19:57:32 +0800

    selection screen

---
+++ app/src/main/java/com/example/mlseriesdemonstrator/activities/SelectionScreenActivity.java
@@ -0,0 +1,38 @@
+package com.example.mlseriesdemonstrator.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import com.example.mlseriesdemonstrator.R;
+import com.example.mlseriesdemonstrator.facial_recognition.FaceRecognitionActivity;
+
+public class SelectionScreenActivity extends AppCompatActivity {
+
+    Button signIn, signUp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selection_screen);
+
+        signIn = findViewById(R.id.SIGN_IN_BTN);
+        signUp = findViewById(R.id.SIGN_UP_BTN);
+
+        signIn.setOnClickListener(v -> startActivity(
+                new Intent(
+                        SelectionScreenActivity.this,
+                        FaceRecognitionActivity.class
+                )
+        ));
+
+        signUp.setOnClickListener(v -> startActivity(
+                new Intent(
+                        SelectionScreenActivity.this,
+                        FaceRecognitionActivity.class
+                )
+        ));
+    }
+}
--- app/src/main/java/com/example/mlseriesdemonstrator/activities/SplashScreenActivity.java
+++ app/src/main/java/com/example/mlseriesdemonstrator/activities/SplashScreenActivity.java
@@ -2,21 +2,59 @@ package com.example.mlseriesdemonstrator.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.example.mlseriesdemonstrator.R;
+import com.example.mlseriesdemonstrator.facial_recognition.FaceRecognitionActivity;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
+@SuppressLint("CustomSplashScreen")
 public class SplashScreenActivity extends AppCompatActivity {
 
-    ProgressBar circleProgressBar, horizontalProgressBar;
+    ProgressBar horizontalProgressBar;
+    TextView loadingText;
+    int counter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
-        circleProgressBar = findViewById(R.id.PROGRESS_BAR_CIRCLE);
-        horizontalProgressBar = findViewById(R.id.PROGRESS_BAR_HORIZONTAL);
+        start_load();
+
+        Handler h = new Handler();
+        h.postDelayed(() -> {
+            startActivity(new Intent(SplashScreenActivity.this, SelectionScreenActivity.class));
+            finish();
+        }, 8000);
     }
+
+    public void start_load() {
+
+        loadingText = findViewById(R.id.LOADING_TEXT);
+        horizontalProgressBar = findViewById(R.id.PROGRESS_BAR_HORIZONTAL);
+        Timer timer = new Timer();
+
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                counter++;
+                horizontalProgressBar.setProgress(counter);
+
+                if(counter == 100){
+                    timer.cancel();
+                }
+            }
+        };
+
+        timer.schedule(timerTask, 0, 100);
+    }
+
 }
