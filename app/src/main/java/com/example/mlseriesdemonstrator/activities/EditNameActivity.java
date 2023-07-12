4607c6afcf286bb5c0d31d701aab41b966b5e54d
gian <michaelgiantiqui3@gmail.com>
2023-07-12 14:36:45 +0800

    new activities

---
+++ app/src/main/java/com/example/mlseriesdemonstrator/activities/EditNameActivity.java
@@ -0,0 +1,14 @@
+package com.example.mlseriesdemonstrator.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.mlseriesdemonstrator.R;
+
+public class EditNameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_name);
+    }
+}
