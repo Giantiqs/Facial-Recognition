44d73b2d5f6e8afaeac5c0e54da3bcee8975f801
gian <michaelgiantiqui3@gmail.com>
2023-08-27 21:43:46 +0800

    naur

--- app/src/main/java/com/example/mlseriesdemonstrator/MainActivity.java
+++ app/src/main/java/com/example/mlseriesdemonstrator/MainActivity.java
@@ -24,6 +24,7 @@ import com.google.firebase.auth.FirebaseUser;
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
+    // naur
     User user;
 
     @SuppressLint("NonConstantResourceId")
