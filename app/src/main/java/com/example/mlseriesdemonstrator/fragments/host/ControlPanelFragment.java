650b5881c438343ef8e9592bbba0edfdf547dc9a
gian <michaelgiantiqui3@gmail.com>
2023-08-30 22:07:42 +0800

    Designed Control Panel

--- app/src/main/java/com/example/mlseriesdemonstrator/fragments/host/ControlPanelFragment.java
+++ app/src/main/java/com/example/mlseriesdemonstrator/fragments/host/ControlPanelFragment.java
@@ -1,5 +1,6 @@
 package com.example.mlseriesdemonstrator.fragments.host;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,10 +8,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.mlseriesdemonstrator.R;
+import com.example.mlseriesdemonstrator.activities.host.SchedulerActivity;
 
 public class ControlPanelFragment extends Fragment {
+
+    Button eventBtn;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -27,7 +32,11 @@ public class ControlPanelFragment extends Fragment {
                 false
         );
 
+        eventBtn = view.findViewById(R.id.SCHEDULE_EVENT);
 
+        eventBtn.setOnClickListener(v -> startActivity(
+                new Intent(getActivity(), SchedulerActivity.class))
+        );
 
         return view;
     }
