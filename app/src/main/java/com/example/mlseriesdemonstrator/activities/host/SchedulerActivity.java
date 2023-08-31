a9ddd54da018e50c3cd31ef5f6d378232b6e23ca
gian <michaelgiantiqui3@gmail.com>
2023-08-31 13:10:38 +0800

    Initial code for SchedulerActivity

--- app/src/main/java/com/example/mlseriesdemonstrator/activities/host/SchedulerActivity.java
+++ app/src/main/java/com/example/mlseriesdemonstrator/activities/host/SchedulerActivity.java
@@ -3,15 +3,31 @@ package com.example.mlseriesdemonstrator.activities.host;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.mlseriesdemonstrator.R;
 
 public class SchedulerActivity extends AppCompatActivity {
 
+    EditText eventTitleTxt;
+    EditText eventDateTxt;
+    EditText eventStartTime;
+    EditText locationTxt;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scheduler);
+
+        eventTitleTxt = findViewById(R.id.EVENT_TITLE);
+        eventDateTxt = findViewById(R.id.EVENT_DATE);
+        eventStartTime = findViewById(R.id.EVENT_TIME);
+        locationTxt = findViewById(R.id.EVENT_LOCATION);
+
+        locationTxt.setOnClickListener(v -> {
+            // Add geofence functions
+        });
 
 
     }
