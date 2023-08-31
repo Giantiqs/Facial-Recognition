5076165f5b8de82659fa57c5184447fb54d93101
gian <michaelgiantiqui3@gmail.com>
2023-08-31 11:34:42 +0800

    Event model added

---
+++ app/src/main/java/com/example/mlseriesdemonstrator/model/Event.java
@@ -0,0 +1,25 @@
+package com.example.mlseriesdemonstrator.model;
+
+import android.location.Location;
+
+import com.google.type.Date;
+
+import java.sql.Time;
+
+public class Event {
+
+    /*
+
+        THESE VARIABLES ARE SUBJECT TO CHANGE LATER
+        SO PUT THE GETTER AND SETTER LATER OKE TY
+
+     */
+
+    private int ID;
+    private String title;
+    private Date date;
+    private Time startTime;
+    private Time endTime;
+    private Location location;
+
+}
