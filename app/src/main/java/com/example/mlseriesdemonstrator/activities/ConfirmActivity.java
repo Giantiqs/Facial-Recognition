f952c6a8b745aea72d57e1b376c6e8929e142cfd
gian <michaelgiantiqui3@gmail.com>
2023-09-23 22:47:36 +0800

    ||||

--- app/src/main/java/com/example/mlseriesdemonstrator/activities/ConfirmActivity.java
+++ app/src/main/java/com/example/mlseriesdemonstrator/activities/ConfirmActivity.java
@@ -14,16 +14,13 @@ import com.example.mlseriesdemonstrator.utilities.Utility;
 
 public class ConfirmActivity extends AppCompatActivity {
 
-    private Button confirm;
-    private Button cancel;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirm);
 
-        confirm = findViewById(R.id.CONFIRM);
-        cancel = findViewById(R.id.CANCEL);
+        Button confirm = findViewById(R.id.CONFIRM);
+        Button cancel = findViewById(R.id.CANCEL);
 
         confirm.setOnClickListener(v -> {
             confirmed();
