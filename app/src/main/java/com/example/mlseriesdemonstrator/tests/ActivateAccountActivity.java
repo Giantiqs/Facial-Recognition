48889689b474786e6b4c34417f6857ad086abf84
gian <michaelgiantiqui3@gmail.com>
2023-09-27 00:59:55 +0800

    try

--- app/src/main/java/com/example/mlseriesdemonstrator/tests/ActivateAccountActivity.java
+++ app/src/main/java/com/example/mlseriesdemonstrator/tests/ActivateAccountActivity.java
@@ -2,15 +2,49 @@ package com.example.mlseriesdemonstrator.tests;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.mlseriesdemonstrator.R;
 
+import java.security.interfaces.EdECKey;
+
 public class ActivateAccountActivity extends AppCompatActivity {
+
+    Context context;
+    EditText studentNumberInput;
+    Button idFinderBtn;
+    TextView fullnameTxt;
+    TextView studentNumberTxt;
+    TextView courseTxt;
+    Student student;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activate_account);
+        RegisterTest.addStudents();
+        context = ActivateAccountActivity.this;
+        studentNumberInput = findViewById(R.id.STUDENT_NUMBER);
+        idFinderBtn = findViewById(R.id.CHECK_STUDENT);
+        fullnameTxt = findViewById(R.id.FULL_NAME);
+        studentNumberTxt = findViewById(R.id.STUDENT_NUMBER_TXT);
+        courseTxt = findViewById(R.id.COURSE_TXT);
+
+        idFinderBtn.setOnClickListener(v -> {
+            String studentIdStr = studentNumberInput.getText().toString();
+
+            student = RegisterTest.getStudentById(studentIdStr, context);
+
+            String fullname = student.getFirstName() + " " + student.getLastName();
+
+            fullnameTxt.setText(fullname);
+            studentNumberTxt.setText(student.getStudentID());
+            courseTxt.setText(student.getCourse());
+        });
+
     }
 }
