d55f995f90dd33715897d4b780e56a693ebc6771
gian <michaelgiantiqui3@gmail.com>
2023-10-12 01:10:51 +0800

    new host activities added

---
+++ app/src/main/java/com/example/mlseriesdemonstrator/activities/host/EditEventActivity.java
@@ -0,0 +1,16 @@
+package com.example.mlseriesdemonstrator.activities.host;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mlseriesdemonstrator.R;
+
+public class EditEventActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_edit_event);
+  }
+}
