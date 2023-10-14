23a2fa88b68ee60d518185d87cb77dd1f7ce8d1e
gian <michaelgiantiqui3@gmail.com>
2023-10-14 10:27:04 +0800

    edit event pls

--- app/src/main/java/com/example/mlseriesdemonstrator/activities/host/EditEventActivity.java
+++ app/src/main/java/com/example/mlseriesdemonstrator/activities/host/EditEventActivity.java
@@ -1,16 +1,26 @@
 package com.example.mlseriesdemonstrator.activities.host;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.example.mlseriesdemonstrator.R;
 
 public class EditEventActivity extends AppCompatActivity {
 
+  private static final String TAG = "EditEventActivity";
+  RecyclerView eventsRecyclerView;
+  Button backBtn;
+  Context context;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_edit_event);
+
+    // reuse the dialog and add a restart event btn if the event is cancelled
   }
 }
