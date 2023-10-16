9a31586d18f802171006737b9b42e595441e6146
gian <michaelgiantiqui3@gmail.com>
2023-10-16 18:17:16 +0800

    logo!!!!!!!!!!

--- app/src/main/java/com/example/mlseriesdemonstrator/fragments/student/HomeFragment.java
+++ app/src/main/java/com/example/mlseriesdemonstrator/fragments/student/HomeFragment.java
@@ -2,22 +2,17 @@ package com.example.mlseriesdemonstrator.fragments.student;
 
 import android.content.Context;
 import android.os.Bundle;
-
-import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.mlseriesdemonstrator.R;
 import com.example.mlseriesdemonstrator.adapter.EventAdapter;
-import com.example.mlseriesdemonstrator.model.Event;
 import com.example.mlseriesdemonstrator.model.User;
 import com.example.mlseriesdemonstrator.utilities.EventManager;
 import com.example.mlseriesdemonstrator.utilities.Utility;
@@ -53,7 +48,8 @@ public class HomeFragment extends Fragment {
         eventsRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         eventsRecyclerView.setAdapter(new EventAdapter(getContext(), events));
       } else {
-        Log.d(TAG, "onCreateView: meow");
+
+        Log.d(TAG, "no events rn fr");
       }
     });
 
